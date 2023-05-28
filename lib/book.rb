ea430ae999fa1b4d88327c10bf686c3e26f107ea
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-05-28 12:43:03 +0300

    lab done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+  attr_writer :title
+  attr_accessor :title,:author_name,:page_count,:genre
+
+  def initialize(title,author_name,page_count,genre)
+    @title = "And Then There Were None"
+    @author_name = author_name
+    @page_count = page_count
+    @genre = genre
+    
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+
+
+end
+
+book_1 = Book.new("And Then There Were None","Agatha Christie",272,"Mystrey")
+puts book_1.title
+puts book_1.author_name
+puts book_1.page_count
+puts book_1.genre
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand,:color,:size,:material,:condition
+
+  def initialize(brand)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+  
+
+end
+
+
+shoe_1 = Shoe.new("Adidas","red",9.5,"suede","tattered")
